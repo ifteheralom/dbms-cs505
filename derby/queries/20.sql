dc0294a93aff78bc96e655429123dcbaade7d384
ifteheralom <ifteher.alom@gmail.com>
2024-04-26 11:15:07 -0400

    update sql

--- derby/queries/20.sql
+++ derby/queries/20.sql
@@ -1,37 +1,40 @@
 select
 	s_name,
-	s_address
+	count(*) as numwait
 from
 	supplier,
+	lineitem l1,
+	orders,
 	nation
 where
-	s_suppkey in (
+	s_suppkey = l1.l_suppkey
+	and o_orderkey = l1.l_orderkey
+	and o_orderstatus = 'F'
+	and l1.l_receiptdate > l1.l_commitdate
+	and exists (
 		select
-			ps_suppkey
+			*
 		from
-			partsupp
+			lineitem l2
 		where
-			ps_partkey in (
-				select
-					p_partkey
-				from
-					part
-				where
-					p_name like 'chocolate%'
-			)
-			and ps_availqty > (
-				select
-					0.5 * sum(l_quantity)
-				from
-					lineitem
-				where
-					l_partkey = ps_partkey
-					and l_suppkey = ps_suppkey
-					and l_shipdate >= date('1995-01-01')
-					and l_shipdate < CAST({fn TIMESTAMPADD(SQL_TSI_YEAR, 1, CAST('1995-01-01 00:00:00' AS TIMESTAMP))} AS DATE)
-			)
+			l2.l_orderkey = l1.l_orderkey
+			and l2.l_suppkey <> l1.l_suppkey
+	)
+	and not exists (
+		select
+			*
+		from
+			lineitem l3
+		where
+			l3.l_orderkey = l1.l_orderkey
+			and l3.l_suppkey <> l1.l_suppkey
+			and l3.l_receiptdate > l3.l_commitdate
 	)
 	and s_nationkey = n_nationkey
-	and n_name = 'CANADA'
+	and n_name = 'EGYPT'
+group by
+	s_name
 order by
-	s_name;
+	numwait desc,
+	s_name
+fetch first 100 rows only;
